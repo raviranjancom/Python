34423e0dcd5b0e787dda133f84e6d097ef24c9bf
Ravi Ranjan <137782361+raviranjancom@users.noreply.github.com>
2024-06-10 12:07:39 +0530

    Update client.py

--- Day01/client.py
+++ Day01/client.py
@@ -4,7 +4,7 @@ HEADER=8
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE="!DISCONNECT"
-SERVER = "192.168.56.1"
+SERVER = socket.gethostbyname(socket.gethostname())
 ADDR=(SERVER,PORT)
 
 client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
