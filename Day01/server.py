d6d36ff78b76eaadf41fbe799294d5b28f6bcacf
Ravi Ranjan <137782361+raviranjancom@users.noreply.github.com>
2024-06-10 11:15:10 +0530

    Add files via upload
    This is a basic client to server python sockets based script.
    It only allows to send one way messages (From client to server).

---
+++ Day01/server.py
@@ -0,0 +1,38 @@
+import socket
+import threading
+
+PORT=5050
+SERVER="192.168.56.1"     #server=socket.gethostbyname(socket.gethostname())    ### IPV4
+ADDR=(SERVER,PORT)
+HEADER=8
+FORMAT='utf-8'
+DISCONNECT_MESSAGE="!DISCONNECT"
+
+server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_clint(conn,addr):
+    print(f"[NEW CONNECTION] {addr} connected....")
+    connected=True
+    while connected:
+        msg_length=conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length=int(msg_length)
+            msg=conn.recv(msg_length).decode(FORMAT)
+            if msg==DISCONNECT_MESSAGE:
+                connected=False
+            print(f"[{addr}] {msg}")
+            conn.send("msg received".encode(FORMAT))
+    conn.close()
+
+
+def start():
+    server.listen()
+    print(f"[LISTENING] server is listening on {SERVER}")
+    while True:
+        conn,addr = server.accept()
+        thread=threading.Thread(handle_clint(conn,addr))    ##(target=handle_clint,args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTION] {threading.active_count()-1}")
+print("[STARTING] SERVER IS STARTING ........ ")
+start()
